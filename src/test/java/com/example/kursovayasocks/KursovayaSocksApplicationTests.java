3acd4458103c3c3088c40854624b2c6db8aff020
user <yulechkaakopyan@yandex.ru>
2023-03-09 13:58:14 +0300

    Проект создан

---
+++ src/test/java/com/example/kursovayasocks/KursovayaSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kursovayasocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KursovayaSocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
